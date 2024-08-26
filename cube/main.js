0e8784f9e57a23264cd3f1f6e0c2a7dfdde688f1
saarthack <ssarthak2003@outlook.com>
2024-08-26 17:37:17 +0530

    cube01

--- cube/main.js
+++ cube/main.js
@@ -1,12 +1,12 @@
 import * as THREE from './three.module.min';  
 
 
-const scene = THREE.Scene()
+const scene =new THREE.Scene()
 
 
 // This is for creating a mesh 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "purple" });
+const material = new THREE.MeshBasicMaterial({ color: "red" });
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
@@ -23,14 +23,14 @@ const camera = new THREE.PerspectiveCamera(
   2000
 ); // near value is 1, and far value is 2000
 camera.position.z = 3;
-camera.position.x = 1;
-camera.position.y = 1;
+// camera.position.x = 1;
+// camera.position.y = 1;
 scene.add(camera);
 
 
 // this is for creating a Renderer
 
-const canvas = document.querySelector(".draw"); //select the canvas element
+const canvas = document.querySelector('canvas'); //select the canvas element
 const renderer = new THREE.WebGLRenderer({ canvas }); //add the WebGLRenderer
 renderer.setSize(aspect.width, aspect.height); //Renderer size
 renderer.render(scene, camera); //display what the camera in the scene captured
